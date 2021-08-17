77bfa23c6f99d0aa54d5a5c6b7c3ead2a3773f29
tuanlh1908developer <85802693+tuanlh1908developer@users.noreply.github.com>
2021-08-17 10:23:59 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,20 +4,8 @@ import (
 	"github.com/tuanlh1908developer/webFilmCrawler/crawler"
 	"github.com/tuanlh1908developer/webFilmCrawler/todb"
 )
-
-//write a csv file, write the header to that file
-//define a collector
-//define onHTML
-//define onError
-//define onResponse
-//define onRequest
-//craw the web
-
 func main() {
 	db := todb.ConnectDB()
 	todb.CreateTable(db)
 	crawler.Crawl(db)
-	// log.Printf("Scraping finished, check file %q for results\n", fName)
-	// c1.Wait()
-	// c2.Wait()
 }
